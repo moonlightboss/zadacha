e900ef889987480368e80275e56994e153ff08e0
moonlightboss <63856802+moonlightboss@users.noreply.github.com>
2020-06-03 15:14:28 +0600

    Update index.php

--- index.php
+++ index.php
@@ -16,15 +16,15 @@ try{
 
 	$conn->exec($sql);
     echo "Таблица создана ";
-    $sql= "INSERT INTO `tablica` (`id`, `date`, `text`) VALUES ('1', '2020-05-04', 'Хорошая погода')";
+    $sql= "INSERT INTO `tablica` (`id`, `date`, `text`) VALUES ('1', '2020-06-03', 'Хорошая погода')";
     $conn->exec($sql);
     echo " Запись 1 добавлена";
 
-    $sql= "INSERT INTO `tablica` (`id`, `date`, `text`) VALUES ('2', '2000-04-14', 'создал табличку')";
+    $sql= "INSERT INTO `tablica` (`id`, `date`, `text`) VALUES ('2', '2000-02-01', 'создал табличку')";
     $conn->exec($sql);
     echo " Запись 2 добавлена";
 
-    $sql= "INSERT INTO `tablica` (`id`, `date`, `text`) VALUES ('3', '1999-12-31', 'и заполнил')";
+    $sql= "INSERT INTO `tablica` (`id`, `date`, `text`) VALUES ('3', '1999-01-23', 'и заполнил')";
     $conn->exec($sql);
     echo " Запись 3 добавлена";
 	
